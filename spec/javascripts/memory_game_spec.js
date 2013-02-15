a2b8ef2c9f02e909a411421934b3784dce4e31fa
viniciustelesbezerra <viniciusteles.bez@gmail.com>
2013-02-15 18:58:20 -0200

    Sending new samples to project

--- spec/javascripts/memory_game_spec.js
+++ spec/javascripts/memory_game_spec.js
@@ -1,7 +1,29 @@
 describe("Memory Game", function () {
-  it("eql sample", function() {
-    loadFixtures("index.html");
-    expect(startGame.x()).toEqual("dsds");
-  });
+	beforeEach(function() {
+	    spyOn(window, 'gameOver');
+	    window.gameOver();
+	});
+
+	//loadFixtures("index.html");
+
+	it("var choose", function() {
+	    expect(choose).toEqual(0);
+	});
+
+	it("var done", function() {
+	    expect(done).toBeDefined();
+	});
+
+	it("array current", function() {
+	    expect(current).toEqual([]);
+	});
+
+	it("array array", function() {
+	    expect(array).toContain(1,2,3);
+	});
+
+	it("gameOver function", function() {
+	    expect(window.gameOver).toHaveBeenCalled();
+	});
 
 });
