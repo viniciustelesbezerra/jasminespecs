4ffb5be93a9722f7e65dd19f54a9a6ccb1cd78bc
Vinicius Teles <viniciusteles.bez@gmail.com>
2013-02-15 00:13:41 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jasminetest::Application.config.secret_token = 'effecfe74f2ae70d76b8187941e282b4e184e9ffc3b99612f80aaf6a1e929cdd1bf6eefde870f2e0a614c4abcecad2c66286830a0e94112c6e21e94c958979cf'
