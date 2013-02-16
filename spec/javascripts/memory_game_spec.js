419ab16957d6c543b600e36e607cd13ea7270761
Vinicius Teles <viniciusteles.bez@gmail.com>
2013-02-15 22:33:49 -0200

    Sending specs to click event

--- spec/javascripts/memory_game_spec.js
+++ spec/javascripts/memory_game_spec.js
@@ -1,121 +1,97 @@
 describe("Memory Game", function () {
 	beforeEach(function() {
-	    loadFixtures('index.html');
+	  loadFixtures('index.html');
 	});
 
-	it("var choose", function() {
-	    expect(choose).toEqual(0);
-	});
-
-	it("var done", function() {
-	    expect(done).toBeDefined();
-	});
-
-	it("array current", function() {
-	    expect(current).toEqual([]);
-	});
-
-	it("array array", function() {
-	    expect(array).toContain(1,2,3);
+	it("variables", function() {
+	  expect(choose).toEqual(0);
+	  expect(done).toBeDefined();
+	  expect(current).toEqual([]);
+	  expect(array).toContain(1,2,3);
 	});
 
 	describe("Function gameOver", function () {
 		beforeEach(function() {
-		    spyOn(window, 'gameOver');
-		    window.gameOver();
+		  spyOn(window, 'gameOver');
+		  window.gameOver();
 		});
 
 		it("tracks that the spy was called", function() {
-		    expect(window.gameOver).toHaveBeenCalled();
+	    expect(window.gameOver).toHaveBeenCalled();
+	   	expect(window.gameOver.calls.length).toEqual(1);
+	   	expect(window.gameOver).toHaveBeenCalledWith();
 		});
 
-		it("tracks its number of calls", function() {
-	    	expect(window.gameOver.calls.length).toEqual(1);
-	  	});
+  });
 
-	  	it("tracks all the arguments of its calls", function() {
-	    	expect(window.gameOver).toHaveBeenCalledWith();
-	  	});
-
-  	});
 
 	describe("Object startGame.init WITH Spy", function () {
 		beforeEach(function() {
-		    spyOn(startGame, 'init');
-	    	startGame.init();
+	    spyOn(startGame, 'init');
+	   	startGame.init();
 		});
 
 		it("tracks that the spy was called", function() {
-		    expect(startGame.init).toHaveBeenCalled();
+	    expect(startGame.init).toHaveBeenCalled();
+	   	expect(startGame.init.calls.length).toEqual(1);
+	   	expect(startGame.init).toHaveBeenCalledWith();
 		});
-
-		it("tracks its number of calls", function() {
-	    	expect(startGame.init.calls.length).toEqual(1);
-	  	});
-
-	  	it("tracks all the arguments of its calls", function() {
-	    	expect(startGame.init).toHaveBeenCalledWith();
-	  	});
 		
-  	});
+ 	});
 
 	describe("Object startGame.init WITHOUT Spy", function () {
 		beforeEach(function() {
-		    spyOn(startGame, 'sortCards');
-		    startGame.init();
+		  spyOn(startGame, 'sortCards');
+		  startGame.init();
 		});
 
-		it("var done", function () {
+		it("variables", function () {
 			expect(done).toEqual(0);
-		});
-
-		it("array array not Equal origin", function () {
 			expect(array).toNotEqual([1,2,3,1,2,3]);
 		});
 
 		it("tracks that the spy was called", function() {
-		    expect(startGame.sortCards).toHaveBeenCalled();
+	    expect(startGame.sortCards).toHaveBeenCalled();
+    	expect(startGame.sortCards.calls.length).toEqual(1);
+    	expect(startGame.sortCards).toHaveBeenCalledWith();
 		});
 
-		it("tracks its number of calls", function() {
-	    	expect(startGame.sortCards.calls.length).toEqual(1);
-	  	});
+  	it("$('div.numbers').click", function() {
+			var spyEvent = spyOnEvent('div.numbers', 'click');
+			$("div.numbers").click(); 
+			expect('click').toHaveBeenTriggeredOn('div.numbers');
+			expect(spyEvent).toHaveBeenTriggered();
+  	});
 
-	  	it("tracks all the arguments of its calls", function() {
-	    	expect(startGame.sortCards).toHaveBeenCalledWith();
-	  	});
-
-	  	//$("div.numbers").click
+  	it("$('div.numbers').click", function() {
+		  spyOn(startGame, 'selectCards');
+			$("div.numbers").click(startGame.selectCards());
+			expect(startGame.selectCards).toHaveBeenCalled();
+  	});
 	  	
 	});
 
 	describe("Object startGame.sortCards WITH spy", function () {
 		beforeEach(function() {
-		    spyOn(startGame, 'sortCards');
-	    	startGame.sortCards();
+	    spyOn(startGame, 'sortCards');
+    	startGame.sortCards();
 		});
 
 		it("tracks that the spy was called", function() {
-		    expect(startGame.sortCards).toHaveBeenCalled();
+	    expect(startGame.sortCards).toHaveBeenCalled();
+	   	expect(startGame.sortCards.calls.length).toEqual(1);
+	    expect(startGame.sortCards).toHaveBeenCalledWith();
 		});
 
-		it("tracks its number of calls", function() {
-	    	expect(startGame.sortCards.calls.length).toEqual(1);
-	  	});
+ 	});
 
-	  	it("tracks all the arguments of its calls", function() {
-	    	expect(startGame.sortCards).toHaveBeenCalledWith();
-	  	});
-
-  	});
-
-  	describe("Object startGame.sortCards WITHOUT spy", function () {
+ 	describe("Object startGame.sortCards WITHOUT spy", function () {
 		beforeEach(function() {
-	    	startGame.sortCards();
+    	startGame.sortCards();
 		});
 
 		it("$('div#content')", function() {
-		    expect($("div#content").html()).toBe("");
+	    expect($("div#content").html()).toBe("");
 		});
 
 	});
